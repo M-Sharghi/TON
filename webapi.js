0af9b546cd40d2960b5a6c963f484c1a4390cfc7
mahtab sharghi <sharghi.mahtab@gmail.com>
2022-05-26 18:39:57 +0430

    changed container

--- webapi.js
+++ webapi.js
@@ -3,7 +3,6 @@ function transaction_interval(){
     let transaction_element=document.querySelector(".statistics-amount-first");
     let transaction_value=transaction_element.textContent;
     transaction_element.textContent=Number(transaction_value) + 1;
-    console.log(transaction_element.textContent);
 }
 setInterval(transaction_interval,1000);
 
@@ -13,6 +12,5 @@ function wallet_interval(){
     let wallet_element=document.querySelector(".statistics-amount-second");
     let wallet_value=wallet_element.textContent;
     wallet_element.textContent=Number(wallet_value) + 1;
-    console.log(wallet_element.textContent);
 }
 setInterval(wallet_interval,30000);
